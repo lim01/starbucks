6beb10709958ebc03030ab0c2d26fc9202cf78b6
lim01 <lim01@naver.com>
2021-04-04 14:26:42 +0900

    VISUAL

--- js/main.js
+++ js/main.js
@@ -34,3 +34,18 @@ window.addEventListener(
     }
   }, 300)
 );
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
+
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true,
+});
